e9910bdd7a083cdeda1c0745de64da5532bac9e3
Gregoire <barret.gregoire@gmail.com>
2022-01-15 22:08:52 +0700

    Add prod

---
+++ server.js
@@ -0,0 +1,17 @@
+const { createServer } = require("https");
+const { parse } = require("url");
+const next = require("next");
+const fs = require("fs");const dev = process.env.NODE_ENV !== "production";
+const app = next({ dev });
+const handle = app.getRequestHandler();const httpsOptions = {
+  key: fs.readFileSync("/etc/letsencrypt/live/election.gregb.info/fullchain.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/election.gregb.info/privkey.pem"),
+};app.prepare().then(() => {
+  createServer(httpsOptions, (req, res) => {
+    const parsedUrl = parse(req.url, true);
+    handle(req, res, parsedUrl);
+  }).listen(8092, (err) => {
+    if (err) throw err;
+    console.log("> Server started on https://localhost:8092");
+  });
+});
